3c784c0bc846141b2e3eff3b3fd82ec3e4281fae
lg-freire <l-gabriel@outlook.com>
2022-10-16 16:03:44 -0300

    Ex 1005

---
+++ Beginner/BEE 1005/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	double a{}, b{};
+	cin >> a;
+	cin >> b;
+	double avg = ((a * 3.5) + (b * 7.5)) / 11;
+	printf("MEDIA = %.4f", avg);
+	cout << '\n';
+}
