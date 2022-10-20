02630fbd8f33a89fef87a03b7d1cef08e31331f1
lg-freire <l-gabriel@outlook.com>
2022-10-20 17:51:50 -0300

    Ex 1011

---
+++ Beginner/BEE 1011/source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	double radius{};
+	cin >> radius;
+
+	printf("VOLUME = %.3f", ((4.0 / 3) * 3.14159 * pow(radius, 3)));
+}
