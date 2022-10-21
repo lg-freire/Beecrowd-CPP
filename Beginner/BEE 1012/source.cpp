ab1bca189671e6b51662cdfb53277920f6966f11
lg-freire <l-gabriel@outlook.com>
2022-10-21 19:33:12 -0300

    Ex 1012

--- Beginner/BEE 1012/source.cpp
+++ Beginner/BEE 1012/source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-	
+	double a{}, b{}, c{};
+	cin >> a >> b >> c;
+
+	printf("TRIANGULO: %.3f", (a * b) / 2);
+	cout << '\n';
+	printf("CIRCULO: %.3f", (c * c) * 3.14159);
+	cout << '\n';
+	printf("TRAPEZIO: %.3f", ((a + b) / 2) * c);
+	cout << '\n';
+	printf("QUADRADO: %.3f", b * b);
+	cout << '\n';
+	printf("RETANGULO: %.3f", a * b);
+	cout << '\n';
+
+	return 0;
 }
