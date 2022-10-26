0df197a079459098aff66bfb60a3f37b22e8b5d2
lg-freire <l-gabriel@outlook.com>
2022-10-26 17:38:24 -0300

    Ex 1018

--- Beginner/BEE 1018/source.cpp
+++ Beginner/BEE 1018/source.cpp
@@ -9,42 +9,40 @@ int main()
 	if (n >= 100) {
 		n100 = n / 100;
 		n = n - (n100 * 100);
-		std::cout << n100 << " nota(s) de R$ 100,00\n";
 	}
 
 	if (n >= 50) {
 		n50 = n / 50;
 		n = n - (n50 * 50);
-		std::cout << n50 << " nota(s) de R$ 50,00\n";
 	}
 	
 	if (n >= 20) {
 		n20 = n / 20;
 		n = n - (n20 * 20);
-		std::cout << n20 << " nota(s) de R$ 20,00\n";
 	}
 
 	if (n >= 10) {
 		n10 = n / 10;
 		n = n - (n10 * 10);
-		std::cout << n10 << " nota(s) de R$ 10,00\n";
 	}
 
 	if (n >= 5) {
 		n5 = n / 5;
 		n = n - (n5 * 5);
-		std::cout << n5 << " nota(s) de R$ 5,00\n";
 	}
 
 	if (n >= 2) {
 		n2 = n / 2;
 		n = n - (n2 * 2);
-		std::cout << n2 << " nota(s) de R$ 2,00\n";
 	}
 
 	if (n != 0) {
-		std::cout << n << "nota(s) de R$ 1,00\n";
+		n1 = 1;
 	}
+
+	std::cout << n100 << " nota(s) de R$ 100,00\n" << n50 << " nota(s) de R$ 50,00\n" << n20 << " nota(s) de R$ 20,00\n"
+		<< n10 << " nota(s) de R$ 10,00\n" << n5 << " nota(s) de R$ 5,00\n" << n2 << " nota(s) de R$ 2,00\n"
+		<< n1 << " nota(s) de R$ 1,00\n";
 
 	return 0;
 }
