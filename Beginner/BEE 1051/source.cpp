08cd94004e5df60baf242317c82f9d90592d056e
lg-freire <l-gabriel@outlook.com>
2022-11-09 19:50:37 -0300

    Ex 1051

--- Beginner/BEE 1051/source.cpp
+++ Beginner/BEE 1051/source.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cstdio>
+
+float taxes(float n) {
+	if (n <= 2000)
+		return 0;
+	else if (2000 < n && n <= 3000) {
+		n = ((n - 2000) * 0.08);
+		return n;
+	}
+	else if (3000 < n && n <= 4500) {
+		n = 80 + ((n - 3000) * 0.18);
+		return n;
+	}
+	else {
+		n = 350 + ((n - 4500) * 0.28);
+		return n;
+	}
+}
 
 int main()
 {
-	
+	float wage{};
+	std::cin >> wage;
+
+	float final{ taxes(wage) };
+
+	if (final == 0)
+		std::cout << "Isento\n";
+	else {
+		printf("R$ %.2f", final);
+		std::cout << '\n';
+	}
+
+	return 0;
 }
