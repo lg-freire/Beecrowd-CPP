c5103dfa394449ee9bf7ff3add747facb19ca595
lg-freire <l-gabriel@outlook.com>
2022-11-10 16:45:33 -0300

    Ex 1052

---
+++ Beginner/BEE 1052/source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int month{};
+	cin >> month;
+
+	if (month == 1)
+		cout << "January\n";
+	else if (month == 2)
+		cout << "February\n";
+	else if (month == 3)
+		cout << "March\n";
+	else if (month == 4)
+		cout << "April\n";
+	else if (month == 5)
+		cout << "May\n";
+	else if (month == 6)
+		cout << "June\n";
+	else if (month == 7)
+		cout << "July\n";
+	else if (month == 8)
+		cout << "August\n";
+	else if (month == 9)
+		cout << "September\n";
+	else if (month == 10)
+		cout << "October\n";
+	else if (month == 11)
+		cout << "November\n";
+	else if (month == 12)
+		cout << "December\n";
+
+	return 0;
+}
