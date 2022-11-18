fa42f96250c327ae1932761d0f015e1498b53df9
lg-freire <l-gabriel@outlook.com>
2022-11-18 18:44:02 -0300

    Ex 1066

--- Beginner/BEE 1065/source.cpp
+++ Beginner/BEE 1065/source.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
-	
+	int input{}, count{ 0 };
+
+	for (int i = 0; i < 5; i++) {
+		std::cin >> input;
+		if (input % 2 == 0)
+			count = count + 1;
+	}
+
+	std::cout << count << " valores pares\n";
+
+	return 0;
 }
