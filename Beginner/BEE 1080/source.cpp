fc51ae6e7a3aa7be8d7fba37498e70c20036893e
lg-freire <l-gabriel@outlook.com>
2022-11-23 14:26:29 -0300

    Ex 1080

--- Beginner/BEE 1080/source.cpp
+++ Beginner/BEE 1080/source.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
+	int count{ 0 }, input{}, max{};
 	
+	for (int i = 0; i < 100; i++) {
+		if (count == 0) {
+			std::cin >> input;
+			max = input;
+			count++;
+		}
+		else {
+			std::cin >> input;
+			if (input > max)
+				max = input;
+			count++;
+		}
+	}
+
+	std::cout << max << '\n' << count << '\n';
+
+	return 0;
 }
