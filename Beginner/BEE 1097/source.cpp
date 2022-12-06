56ffefa355ae3ad1592f0eb07d0a15e01b587492
lg-freire <l-gabriel@outlook.com>
2022-12-06 15:49:20 -0300

    Ex 1097

---
+++ Beginner/BEE 1097/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int i{ 1 }, j{ 7 };
+
+	while (i <= 9) {
+		for (int n = 0; n < 3; n++) {
+			std::cout << "I=" << i << " J=" << j << '\n';
+			j -= 1;
+		}
+		i += 2;
+		j += 5;
+	}
+
+	return 0;
+}
