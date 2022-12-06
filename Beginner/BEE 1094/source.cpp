53996c3103494958f923c748d0ce62943c18dbee
lg-freire <l-gabriel@outlook.com>
2022-12-06 15:16:38 -0300

    Ex 1094

---
+++ Beginner/BEE 1094/source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	int cases{}, amount{}, b{}, r{}, f{};
+	string type{};
+	cin >> cases;
+
+	for (int i = 0; i < cases; i++) {
+		cin >> amount >> type;
+
+		if (type == "C")
+			b += amount;
+		else if (type == "R")
+			r += amount;
+		else if (type == "S")
+			f += amount;
+	}
+
+	int total{ b + r + f };
+
+	cout << "Total: " << total << " cobaias\n" << "Total de coelhos: " << b << "\nTotal de ratos: " << r << "\nTotal de sapos: " << f << '\n';
+	printf("Percentual de coelhos: %.2f", (b * 100.0) / total);
+	cout << " %" << '\n';
+	// printf("")
+
+	return 0;
+}
